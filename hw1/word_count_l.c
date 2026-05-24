08912e44e1a0b86c40ae427bc3210a9f6a3a0be1
Kulneet Singh <kulneetsingh@vt.edu>
2026-05-24 16:04:18 +0000

    Got pintos list word count working

--- hw1/word_count_l.c
+++ hw1/word_count_l.c
@@ -27,32 +27,68 @@
 #include "word_count.h"
 
 void init_words(word_count_list_t *wclist) {
-  /* TODO */
+  list_init(wclist);
 }
 
 size_t len_words(word_count_list_t *wclist) {
-  /* TODO */
-  return 0;
+  size_t num_words = 0;
+  struct list_elem *e;
+
+  for (e = list_begin (wclist); e != list_end (wclist);
+        e = list_next (e))
+  {
+    num_words++;
+  }
+  return num_words;
 }
 
 word_count_t *find_word(word_count_list_t *wclist, char *word) {
-  /* TODO */
+  struct list_elem *e;
+
+  for (e = list_begin (wclist); e != list_end (wclist);
+        e = list_next (e))
+  {
+    word_count_t *node = list_entry (e, word_count_t, elem);
+    if (strcmp(word, node->word) == 0) return node;
+  }
   return NULL;
 }
 
 word_count_t *add_word(word_count_list_t *wclist, char *word) {
-  /* TODO */
-  return NULL;
+  if (!wclist || !word) return NULL;
+
+  word_count_t *node = find_word(wclist, word);
+  if (node) {
+    node->count++;
+  } else {
+    node = malloc(sizeof(word_count_t));
+    if (!node) return NULL;
+    node->word = strdup(word);
+    if (!node->word) {
+      free(node);
+      return NULL;
+    }
+    node->count = 1;
+    list_push_front(wclist, &node->elem);
+  }
+  return node;
 }
 
 void fprint_words(word_count_list_t *wclist, FILE *outfile) {
-  /* TODO */
+  struct list_elem *e;
+  for (e = list_begin (wclist); e != list_end (wclist);
+        e = list_next (e))
+  {
+    word_count_t *node = list_entry (e, word_count_t, elem);
+    fprintf(outfile, "%i\t%s\n", node->count, node->word);
+  }
 }
 
 static bool less_list(const struct list_elem *ewc1,
                       const struct list_elem *ewc2, void *aux) {
-  /* TODO */
-  return false;
+  word_count_t *node1 = list_entry (ewc1, word_count_t, elem);
+  word_count_t *node2 = list_entry (ewc2, word_count_t, elem);
+  return ((bool (*)(const word_count_t *, const word_count_t *))aux)(node1, node2);
 }
 
 void wordcount_sort(word_count_list_t *wclist,
